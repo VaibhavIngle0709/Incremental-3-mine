358a67c904e5f5abb0e866c50716cfad4ea045a0
Iamneo <techadmin@iamneo.ai>
2023-11-20 11:35:37 +0000

    Backup Commit

--- dotnetapp/Models/ApplicationDbContext.cs
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -3,13 +3,21 @@ using dotnetapp.Models;
 
 namespace dotnetapp.Models
 {
-    public class ApplicationDbContext : DbContext
+  public class ApplicationDbContext : DbContext
     {
+        public virtual DbSet<Player> Players{get;set;}
+        public virtual DbSet<Team> Teams{get;set;}
+        public ApplicationDbContext(){}
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
-        // Add DbSet properties for other entities as needed
-    }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("User ID=sa;password=examlyMssql@123; server=localhost;Database=IPLDb;trusted_connection=false;Persist Security Info=False;Encrypt=False");
+            }
+        }
 }
--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -1,4 +1,21 @@
 ﻿namespace dotnetapp.Models
 {
-    
+     public class Player
+    {
+        public int Id{get;set;}
+
+        [Required(ErrorMessage="Name is required.")]
+        [MaxLength(17)]
+        public string Name{get;set;}
+        public string Category{get;set;}
+
+        [ForeignKey("Team")]
+        public int TeamId{get;set;}
+
+        [Range(1,int.MaxValue,ErrorMessage="Bidding amount must be greater than 0.")]
+        public decimal BiddingAmount{get;set;}
+
+        public Team ? team{get;set;}        
+
+    }
 }
