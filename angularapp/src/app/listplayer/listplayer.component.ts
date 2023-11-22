8198e90c93df05ce03265cc808c243e3e2063a8d
Iamneo <techadmin@iamneo.ai>
2023-11-22 14:21:47 +0000

    Backup Commit

--- angularapp/src/app/listplayer/listplayer.component.ts
+++ angularapp/src/app/listplayer/listplayer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from 'src/models/player.model';
+import { AdminService } from '../services/admin.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-listplayer',
@@ -6,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listplayer.component.css']
 })
 export class ListplayerComponent implements OnInit {
-
-  constructor() { }
+players:Player[]
+  constructor(private as:AdminService,private ar:ActivatedRoute,private route:Router) { }
 
   ngOnInit(): void {
+    this.as.getAllPlayers().subscribe(data=>{this.players.push(...data)});
   }
 
 }
