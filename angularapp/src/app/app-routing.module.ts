e83557d81c1d13707e1ac64507c9a23b21f0aae2
Iamneo <techadmin@iamneo.ai>
2023-11-23 13:23:42 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login1/login/login.component';
+import { LoginComponent } from './login/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo:'/home',pathMatch:'full' },
+  {path:'home',component:HomeComponent},
   { path: 'login', component: LoginComponent }
 ];
 
